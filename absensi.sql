21c00ced366686a1d3a33e9b41e8ad29e331adbd
DESKTOP-2GKF6KT\ASUS <irfiyanda52@gmail.com>
2021-06-04 08:09:39 +0700

    no message

--- absensi.sql
+++ absensi.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 27, 2021 at 02:41 PM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 8.0.0
+-- Generation Time: Jun 04, 2021 at 03:07 AM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `utsintegrasiapi`
+-- Database: `integrasiapi`
 --
 
 -- --------------------------------------------------------
@@ -38,6 +38,16 @@ CREATE TABLE `absensi` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Dumping data for table `absensi`
+--
+
+INSERT INTO `absensi` (`id_absensi`, `nik`, `jabatan_prshn`, `nama`, `alfa`, `izin`, `hadir`) VALUES
+(1, '5102082206920002', 'Karyawan C.2', 'AMERTA CANDRA ERDIKA', 2, 1, 27),
+(4, '1304040605900001', 'Direksi', 'ANDIKO PERDANA', 0, 1, 29),
+(5, '1304040605900011', 'Karyawan B.2', 'DWI ASIH RETNO PRATIWI', 1, 0, 29),
+(6, '1375026004890002', 'Direktur Utama', 'DRACO MALFOY', 0, 0, 30);
+
+--
 -- Indexes for dumped tables
 --
 
@@ -55,7 +65,7 @@ ALTER TABLE `absensi`
 -- AUTO_INCREMENT for table `absensi`
 --
 ALTER TABLE `absensi`
-  MODIFY `id_absensi` int(30) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_absensi` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
